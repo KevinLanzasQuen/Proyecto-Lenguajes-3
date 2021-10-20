a6201ef8a5699e1dff01a08840193d923cd2acf8
KevinLanzasQuen <kevin.lanzas@hotmail.com>
2021-10-20 17:20:40 -0600

    Se creo la ventana inicial del programa y el tablero.

---
+++ SudokuX/src/sudokux/SudokuX.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sudokux;
+package Vistas;
+
+import Vistas.ventanaInicial;
+
+/**
+ *
+ * @author kevin
+ */
+public class SudokuX {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventanaInicial v1 = new ventanaInicial();
+        v1.setLocationRelativeTo(null);
+        v1.setVisible(true);
+    }
+    
+}
